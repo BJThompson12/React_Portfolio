db2d1dc9a54dd56a0e1d07ae7ffdeaad747dab1b
Brandon Thompson <brandonjoel.thompson@gmail.com>
2023-05-13 17:25:57 -0400

    installed vite and created the react app with tailwind

---
+++ myAPP/src/App.jsx
@@ -0,0 +1,10 @@
+//import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline border-solid border-4 border-gray-600 ">
+      Hello world!
+    </h1>
+  )
+}
