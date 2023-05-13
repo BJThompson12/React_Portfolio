821b164498d2eeb68f6301c1c79559a520a2a642
Brandon Thompson <brandonjoel.thompson@gmail.com>
2023-05-13 19:24:24 -0400

    file setup

--- myAPP/src/App.jsx
+++ myAPP/src/App.jsx
@@ -1,10 +1,9 @@
-//import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline border-solid border-4 border-gray-600 ">
-      Hello world!
-    </h1>
-  )
+    <>
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+    </>
+  );
 }
