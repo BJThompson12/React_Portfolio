6b5f1f96b932cea1962ac499852274e8d1890850
Brandon Thompson <brandonjoel.thompson@gmail.com>
2023-06-12 21:32:43 -0400

    added new project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ProjectCards from './ProjectCards'
+import projeCritterImg from '../assets/projeCritter.jpg'
 import animeImg from '../assets/anime.jpg'
 import landscapeImg from '../assets/landscape.jpg'
 import weatherImg from '../assets/weather.jpg'
@@ -16,12 +17,12 @@ const Projects = () => {
       <p className='text-center py-8'>
       </p>
       <div className='grid sm:grid-cols-2 gap-12'>
+        <ProjectCards img={projeCritterImg} title='projeCritter' href='https://projecritter.herokuapp.com/'/>
         <ProjectCards img={animeImg} title='Animatch' href='https://animatch.herokuapp.com/'/>
         <ProjectCards img={landscapeImg} title='Artiscapes' href='https://bjthompson12.github.io/Sea-Wolves/'/>
         <ProjectCards img={weatherImg} title='Weather Dashboard' href='https://bjthompson12.github.io/API-Weather-Dashboard/'/>
         <ProjectCards img={schedulerImg} title='Workday Scheduler' href='https://bjthompson12.github.io/Workday-Daily-Planner/'/>
         <ProjectCards img={passwordImg} title='Password Generator' href='https://bjthompson12.github.io/Password_Generator/'/>
-        <ProjectCards img={comingSoonImg} title='Coming Soon' href='https://www.comingsoon.net/movies/news/1288654-deadpool-3-working-title-unveiled-for-marvel-studios-sequel'/>
       </div>
     </div>
   );
